73bffe97ac4cce7d56262920afd8d151b752de99
Unknown <misc@reactivepony.com>
2017-07-13 10:40:44 +0300

    Linear interpolator modification(3D)

--- source/interp.c
+++ source/interp.c
@@ -12,6 +12,57 @@
 #include "interp.h"
 
 interp_result_t interp_result;
+interp_linear_result_t interp_linear_result;
+
+
+interp_linear_result_t INTERP_Linear3dCalcStep(int32_t destStepX, int32_t destStepY, int32_t destStepZ, int32_t originStepX, int32_t originStepY, int32_t originStepZ, int32_t errX, int32_t errY, int32_t errZ)
+{
+	interp_linear_result.stepX = 0;
+	interp_linear_result.stepY = 0;
+	interp_linear_result.stepZ = 0;
+
+	if(errX < 0)
+	{
+		errX += fabs(destStepY) + fabs(destStepZ);
+
+		if(destStepX >= 0)
+			interp_linear_result.stepX = 1;
+		else
+			interp_linear_result.stepX = -1;
+	}
+	else
+		errX -= fabs(destStepX);
+
+	if(errY < 0)
+	{
+		errY += fabs(destStepX) + fabs(destStepZ);
+
+		if(destStepY >= 0)
+			interp_linear_result.stepY = 1;
+		else
+			interp_linear_result.stepY = -1;
+	}
+	else
+		errY -= fabs(destStepY);
+
+	if(errZ < 0)
+	{
+		errZ += fabs(destStepX) + fabs(destStepY);
+
+		if(destStepZ >= 0)
+			interp_linear_result.stepZ = 1;
+		else
+			interp_linear_result.stepZ = -1;
+	}
+	else
+		errZ -= fabs(destStepZ);
+
+	interp_linear_result.errX = errX;
+	interp_linear_result.errY = errY;
+	interp_linear_result.errZ = errZ;
+
+	return interp_linear_result;
+}
 
 interp_result_t INTERP_LinearCalcStep(int32_t destStepA, int32_t destStepB, int32_t originStepA, int32_t originStepB, int32_t F)
 {
