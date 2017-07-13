1a3629f2ebe6e3a91d4d697375c428cc25f29ecb
Unknown <misc@reactivepony.com>
2017-07-13 11:51:22 +0300

    GDB crash -> Linear interpolator name fixed.

--- source/interp.c
+++ source/interp.c
@@ -14,8 +14,7 @@
 interp_result_t interp_result;
 interp_linear_result_t interp_linear_result;
 
-
-interp_linear_result_t INTERP_Linear3dCalcStep(int32_t destStepX, int32_t destStepY, int32_t destStepZ, int32_t originStepX, int32_t originStepY, int32_t originStepZ, int32_t errX, int32_t errY, int32_t errZ)
+interp_linear_result_t INTERP_LinearCalcStep(int32_t destStepX, int32_t destStepY, int32_t destStepZ, int32_t originStepX, int32_t originStepY, int32_t originStepZ, int32_t errX, int32_t errY, int32_t errZ)
 {
 	interp_linear_result.stepX = 0;
 	interp_linear_result.stepY = 0;
@@ -62,69 +61,6 @@ interp_linear_result_t INTERP_Linear3dCalcStep(int32_t destStepX, int32_t destSt
 	interp_linear_result.errZ = errZ;
 
 	return interp_linear_result;
-}
-
-interp_result_t INTERP_LinearCalcStep(int32_t destStepA, int32_t destStepB, int32_t originStepA, int32_t originStepB, int32_t F)
-{
-	interp_result.stepA = 0;
-	interp_result.stepB = 0;
-
-	if(destStepA - originStepA >= 0 && destStepB - originStepB >= 0) // QUADRANT 1
-	{
-		if(F > 0)
-		{
-			F = F - fabsf(destStepB - originStepB);
-			interp_result.stepA = 1;
-		}
-		else
-		{
-			F = F + fabsf(destStepA - originStepA);
-			interp_result.stepB = 1;
-		}
-	}
-	else if(destStepA - originStepA < 0 && destStepB - originStepB >= 0) // QUADRANT 2
-	{
-		if(F > 0)
-		{
-			F = F - fabsf(destStepB - originStepB);
-			interp_result.stepA = -1;
-		}
-		else
-		{
-			F = F + fabsf(destStepA - originStepA);
-			interp_result.stepB = 1;
-		}
-	}
-	else if (destStepA - originStepA < 0 && destStepB - originStepB < 0) // QUADRANT 3
-	{
-		if(F > 0)
-		{
-			F = F - fabsf(destStepB - originStepB);
-			interp_result.stepA = -1;
-		}
-		else
-		{
-			F = F + fabsf(destStepA - originStepA);
-			interp_result.stepB = -1;
-		}
-	}
-	else if(destStepA - originStepA >= 0 && destStepB - originStepB < 0) // QUADRANT 4
-	{
-		if(F > 0)
-		{
-			F = F - fabsf(destStepB - originStepB);
-			interp_result.stepA = 1;
-		}
-		else
-		{
-			F = F + fabsf(destStepA - originStepA);
-			interp_result.stepB = -1;
-		}
-	}
-
-	interp_result.F = F;
-
-	return interp_result;
 }
 
 interp_result_t INTERP_CircleCWCalcStep(int32_t currentStepA, int32_t currentStepB, int32_t centerStepA, int32_t centerStepB, int32_t F)
--- source/interp.h
+++ source/interp.h
@@ -26,8 +26,7 @@ typedef struct _interp_linear_result
 } interp_linear_result_t;
 
 
-interp_linear_result_t INTERP_Linear3dCalcStep(int32_t destStepX, int32_t destStepY, int32_t destStepZ, int32_t originStepX, int32_t originStepY, int32_t originStepZ, int32_t errX, int32_t errY, int32_t errZ);
-interp_result_t INTERP_LinearCalcStep(int32_t destStepA, int32_t destStepB, int32_t originStepA, int32_t originStepB, int32_t F);
+interp_linear_result_t INTERP_LinearCalcStep(int32_t destStepX, int32_t destStepY, int32_t destStepZ, int32_t originStepX, int32_t originStepY, int32_t originStepZ, int32_t errX, int32_t errY, int32_t errZ);
 interp_result_t INTERP_CircleCWCalcStep(int32_t currentStepA, int32_t currentStepB, int32_t centerStepA, int32_t centerStepB, int32_t F);
 interp_result_t INTERP_CircleCCWCalcStep(int32_t currentStepA, int32_t currentStepB, int32_t centerStepA, int32_t centerStepB, int32_t F);
 
